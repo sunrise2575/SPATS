5550b40f5b70cc0380d451be3dbcc2418d2b9f75
Heeyong Yoon <sunrise2575@gmail.com>
2025-02-14 01:58:10 +0900

    fix np.ndarray type error

--- common/format/embed_time.py
+++ common/format/embed_time.py
@@ -15,11 +15,11 @@ __all__ = [
 
 
 def year_of_total(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return (df.year - df.year.min()) / (df.year.max() - df.year.min() + 1)
+    return np.array((df.year - df.year.min()) / (df.year.max() - df.year.min() + 1))
 
 
 def month_of_year(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return df.month / 12
+    return np.array(df.month / 12)
 
 
 def week_of_year(df: CombinedDatetimelikeProperties) -> np.ndarray:
@@ -27,27 +27,28 @@ def week_of_year(df: CombinedDatetimelikeProperties) -> np.ndarray:
 
 
 def day_of_year(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return df.dayofyear / 366
+    return np.array(df.dayofyear / 366)
 
 
 def day_of_month(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return df.day / df.daysinmonth
+    return np.array(df.day / df.daysinmonth)
 
 
 def day_of_week(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return df.dayofweek / 7
+    return np.array(df.dayofweek / 7)
 
 
 def hour_of_day(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return df.hour / 24
+    return np.array(df.hour / 24)
 
 
 def minute_of_day(df: CombinedDatetimelikeProperties) -> np.ndarray:
-    return (df.hour * 60 + df.minute) / (24 * 60)
+    return np.array((df.hour * 60 + df.minute) / (24 * 60))
 
 
 def minute_of_hour(df: CombinedDatetimelikeProperties) -> np.ndarray:
     return df.minute / 60
+
 
 def timestamp_in_day(df: CombinedDatetimelikeProperties) -> np.ndarray:
     T = df.time.unique()
@@ -64,11 +65,12 @@ def timestamp_in_day(df: CombinedDatetimelikeProperties) -> np.ndarray:
         E[i] = II[ts]
     return E
 
+
 def timestamp_in_minute(df: CombinedDatetimelikeProperties) -> np.ndarray:
     T = df.time.unique()
     for i in range(len(T)):
         T[i] = T[i].strftime(':%M:%S.%f')
-    
+
     T = sorted(set(T))
 
     II = {T[i]: i for i in range(len(T))}
@@ -79,6 +81,7 @@ def timestamp_in_minute(df: CombinedDatetimelikeProperties) -> np.ndarray:
         E[i] = II[ts]
 
     return E
+
 
 TEMPORAL_EMBED_FUNC: \
     typing.Dict[str, typing.Callable[[CombinedDatetimelikeProperties], np.ndarray]] \
@@ -103,7 +106,8 @@ def embed_time(tsf: TSF, query: str = None):
     if query is None:
         return
 
-    assert query in TEMPORAL_EMBED_FUNC.keys(), f'Error: query {query} is not supported'
+    assert query in TEMPORAL_EMBED_FUNC.keys(
+    ), f'Error: query {query} is not supported'
 
     # stacking selected normalized temporal info
     df = tsf.T['time'].dt
